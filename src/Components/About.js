11f603e5f9ea9024373986e481d263e5445d3635
oalmaguer <almaguero95@gmail.com>
2020-12-17 02:40:11 -0500

    added github links to projects

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,6 +4,8 @@ import Weather from '../images/weathergif.gif';
 import Mazatlan from '../images/mazatlan.png';
 import Movie from '../images/movie.gif';
 import Node from '../images/nodejs.gif';
+import {AnimatedSocialIcon} from 'react-animated-social-icons';
+import Bamazon from '../images/bamazon.png';
 
 export default class Aboutpage extends Component {
 
@@ -11,19 +13,34 @@ export default class Aboutpage extends Component {
         var arrayWeather = [Weather, Mazatlan];
         var arrayMovie = [Movie];
         var node = [Node];
+        var bamazon = [Bamazon];
         return(
              <div className="cards">
+                 <div>
                 <ProductCard photos={[
             arrayWeather[0],
             arrayWeather[1]
           ]}
           float
           productName='Weather App'
-          description='Weather app I made with React'
+          description='Weather app I made with React and the WeatherAPI'
           buttonText='Go to the live Website'
           url='https://oalmaguer.github.io/weatherApp/'
         />
-
+         <AnimatedSocialIcon
+    brandName="github"
+    url="https://github.com/oalmaguer/weatherApp"
+    animation="bounce"
+    defaultColor="#17bf63"
+    hoverColor="white"
+    width="2em"
+    animationDuration={0.8}
+    style={{padding: '1em', marginBottom: "40px"}}
+    />
+    </div>
+        
+       
+          <div>
 <ProductCard photos={[
             arrayMovie[0]
           ]}
@@ -33,17 +50,61 @@ export default class Aboutpage extends Component {
           buttonText='Go to the live Website'
           url='https://oalmaguer.github.io/movieBook/'
         />
-
+         
+         <AnimatedSocialIcon
+    brandName="github"
+    url="https://github.com/oalmaguer/movieBook"
+    animation="bounce"
+    defaultColor="#17bf63"
+    hoverColor="white"
+    width="2em"
+    animationDuration={0.8}
+    style={{padding: '1em', marginBottom: "40px"}}
+    />
+        </div>
+            <div>
+             
 <ProductCard photos={[
             node[0]
           ]}
           float
-          productName='Node with Spotify API'
-          description='Terminal App to make a call to the Spotify API via a command in the terminal'
+          productName='NodeJS with Spotify API'
+          description='Terminal App with NodeJS to make a call to the Spotify API'
           buttonText='Go to the live Website'
           url='https://github.com/oalmaguer/liri-node-app'
         />
-
+        <AnimatedSocialIcon
+    brandName="github"
+    url="https://github.com/oalmaguer/liri-node-app"
+    animation="bounce"
+    defaultColor="#17bf63"
+    hoverColor="white"
+    width="2em"
+    animationDuration={0.8}
+    style={{padding: '1em', marginBottom: "40px"}}
+    />
+            </div>
+            <div>
+<ProductCard photos={[
+            bamazon[0]
+          ]}
+          float
+          productName='Amazon like App'
+          description='An app that checks the stock of the item you purchase and updates the new quantity.'
+          buttonText='Go to the live Website'
+          url='https://powerful-springs-77120.herokuapp.com/'
+        />
+        <AnimatedSocialIcon
+    brandName="github"
+    url="https://github.com/oalmaguer/bamazon"
+    animation="bounce"
+    defaultColor="#17bf63"
+    hoverColor="white"
+    width="2em"
+    animationDuration={0.8}
+    style={{padding: '1em', marginBottom: "40px"}}
+    />
+            </div>
         </div>
         )
     }
